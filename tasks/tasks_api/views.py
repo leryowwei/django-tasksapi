ce74bef82418f75886aad4fe0eb5cfd74b6604d3
leryowwei <74431259+leryowwei@users.noreply.github.com>
2023-09-07 07:27:08 +0100

    Add categories endpoints (#2)

--- tasks/tasks_api/views.py
+++ tasks/tasks_api/views.py
@@ -7,6 +7,28 @@ from .models import Category, Task
 from .serializers import CategorySerializer, TaskSerializer
 
 
+class CategoriesApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request: Request) -> Response:
+        """List all the categories for the given requested user."""
+        categories = Category.objects.filter(user=request.user.id)
+        category_serializer = CategorySerializer(categories, many=True)
+        return Response(category_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request: Request) -> Response:
+        """Add a category for the given requested user."""
+        category_data = {
+            "category": request.data.get("category"),
+            "user": request.user.id,
+        }
+        category_serializer = CategorySerializer(data=category_data)
+        if category_serializer.is_valid():
+            category_serializer.save()
+            return Response(category_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(category_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class TasksListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
@@ -20,25 +42,18 @@ class TasksListApiView(APIView):
         """Add a task for the given requested user."""
         user_id = request.user.id
 
-        # get category object first. If does not exist, create it on the spot
+        # get category object first. If does not exist, create it via category POST
         category = Category.objects.filter(
             category=request.data.get("category"), user=user_id
         )
         if not category:
-            category_data = {
-                "category": request.data.get("category"),
-                "user": request.user.id,
-            }
-            category_serializer = CategorySerializer(data=category_data)
-            if category_serializer.is_valid():
-                category_serializer.save()
-                category = Category.objects.filter(
-                    category=request.data.get("category"), user=user_id
-                )
-            else:
-                return Response(
-                    category_serializer.errors, status=status.HTTP_400_BAD_REQUEST
-                )
+            category_view = CategoriesApiView()
+            response = category_view.post(request=request)
+            if response.status_code != status.HTTP_201_CREATED:
+                return response
+            category = Category.objects.filter(
+                category=request.data.get("category"), user=user_id
+            )
 
         # create task now
         data = {
@@ -46,7 +61,7 @@ class TasksListApiView(APIView):
             "category": category.first().pk,
             "priority": request.data.get("priority"),
             "status": request.data.get("status"),
-            "user": request.user.id,
+            "user": user_id,
         }
         task_serializer = TaskSerializer(data=data)
         if task_serializer.is_valid():
